bf1adadf35efb57c266c3370ace5c083e210cfc5
napssy <napssy@users.noreply.github.com>
2019-12-16 15:54:32 +0100

    Create onboarding.js

---
+++ easy/onboarding.js
@@ -0,0 +1,7 @@
+while(true){
+    let enemy1 = readline()
+    let dist1 = parseInt(readline())
+    let enemy2 = readline()
+    let dist2 = parseInt(readline())
+    console.log(dist1 < dist2? enemy1 : enemy2)
+}
